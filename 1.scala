a0fb9cffa423353e0e6b6da4d4716a472a7dacc5
Akila Kushantha <akila.kushantha@gmail.com>
2023-09-19 10:31:46 +0530

    Add 3

--- 1.scala
+++ 1.scala
@@ -6,5 +6,5 @@ val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 val output = filterEvenNumbers(input)
 
 def main(args: Array[String]): Unit = {
-    println(output)  // Output: List(2, 4, 6, 8, 10)
+    println(output) 
 }
---
+++ 3.scala
@@ -0,0 +1,12 @@
+object PrimeFilter {
+  def filterPrime(numbers: List[Int]): List[Int] = {
+    numbers.filter(n => (2 until n).forall(i => n % i != 0) && n > 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val primes = filterPrime(input)
+    println("Input: " + input)
+    println("Output: " + primes)
+  }
+}
